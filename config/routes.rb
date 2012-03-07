15151cf9c52cfde241da25b8d0888846d76e7a9b
Carson Tang <tang.carson@gmail.com>
2012-03-07 01:21:42 -0800

    Start with create_remember_token method

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Site::Application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users
-  match '/allusers', to: 'users#index'
-  match '/signup',  to: 'users#new' 
+  
+  match '/allusers',  to: 'users#index'
+  
+  match '/signin',    to: 'sessions#new'
+  match '/signout',   to: 'sessions#destroy', via: :delete
+  match '/signup',    to: 'users#new' 
   
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
