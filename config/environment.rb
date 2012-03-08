f2344882d159bfece085363ae1d8d132fe43d25f
Carson Tang <tang.carson@gmail.com>
2012-03-07 18:43:16 -0800

    Reformatted form for new users

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Site::Application.initialize!
+
+# Customize field error div to work with Bootstrap
+ActionView::Base.field_error_proc = Proc.new do |html, instance|
+  html
+end
