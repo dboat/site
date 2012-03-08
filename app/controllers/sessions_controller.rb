6f2d48fe8581a952dcb8b032fdb87c452744eaf3
Carson Tang <tang.carson@gmail.com>
2012-03-08 10:14:25 -0800

    User edit, update actions available

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,9 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
-      flash[:error] = "Invalid email/password combination"
-      "flash stays even when you switch to a different part of the site"
+      flash.now[:error] = "Invalid email/password combination"
       render 'new'
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user,  only: [:index, :edit, :show, :update]
+  before_filter :correct_user,    only: [:edit, :update]
+  before_filter :admin_user,      only: [:destroy, :index]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -24,7 +28,41 @@ class UsersController < ApplicationController
   
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User deleted."
+    flash[:info] = "User deleted."
     redirect_to users_path
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      
+      # remember_token is reset when user is saved, so we have to sign in user again
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in."
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_path unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
